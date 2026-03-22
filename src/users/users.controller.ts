8b499aa6eaa2d13af6de0c4b98569619a7558ddf
suttirakcha <suttirak.char@gmail.com>
2026-03-22 23:37:47 +0700

    style: remove the createUser log

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,7 +13,6 @@ export class UsersController {
 
   @Post()
   createUser(@Body() createUserDto: CreateUserDto) {
-    console.log(createUserDto);
     return this.usersService.createUser(createUserDto);
   }
 }
