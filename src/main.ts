1d79231e9672329baa7192dd5b833988065cca77
winepnp-code <wine.pnprinz99@gmail.com>
2026-04-06 00:06:00 +0700

    chore: finalize payment api configuration

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { rawBody: true });
 
   app.useGlobalPipes(
     new ValidationPipe({
--- src/upload/cloudinary.service.ts
+++ src/upload/cloudinary.service.ts
@@ -1,9 +1,10 @@
 import 'dotenv/config';
 import { v2 as cloudinary } from 'cloudinary';
+
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
 });
 
 export default cloudinary;
